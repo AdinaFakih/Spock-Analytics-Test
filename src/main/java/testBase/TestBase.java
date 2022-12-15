42afcc5051d5ab5393624db826046d1b0f8efdf1
AdinaFakih <addy.fakih@gmail.com>
2022-12-15 17:24:42 +0500

    Maven project structure

---
+++ src/main/java/testBase/TestBase.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+/**
+ * @author Adina
+ *
+ */
+package testBase;
+
+public class TestBase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
